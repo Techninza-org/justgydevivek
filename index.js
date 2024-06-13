64d36ec443a81f9a87cd4f0f58c0f1611f60ec4d
Vivek <vivekpalvp0770@gmail.com>
2024-06-13 13:06:54 +0530

    0000

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 //server start
     const PORT = process.env.PORT || 5000;
-    app.listen(5000, () => {
+    app.listen(5000, '0.0.0.0', () => {
         console.log(`Server is running on port ${PORT}`);
     });
