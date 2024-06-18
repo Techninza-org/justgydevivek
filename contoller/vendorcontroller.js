6c3f0f9d7cd134ddd25e128a1fa186c014d665b3
Vivek <vivekpalvp0770@gmail.com>
2024-06-18 12:50:14 +0530

    update

--- contoller/vendorcontroller.js
+++ contoller/vendorcontroller.js
@@ -16,7 +16,8 @@ const SALT_ROUND=10;
 // Configure multer to store files on disk
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/');
+    //   cb(null, '../uploads/');
+    cb(null, path.join(__dirname, '../uploads/'));
     },
     filename: (req, file, cb) => {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
